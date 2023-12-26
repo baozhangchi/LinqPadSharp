21d888af6d883e5716095a16c5d4863d0d46dd35
baozhangchi <baozhangchi@live.com>
2023-12-26 18:17:51 +0800

    Create Project

---
+++ LinqPadSharp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LinqPadSharp.ViewModels
+{
+	public class ViewModelBase : ObservableObject
+	{
+	}
+}
---
+++ LinqPadSharp/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LinqPadSharp.Views
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
